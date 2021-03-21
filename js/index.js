3e77b4e82de2ad814cd5ea27d39f2ecc68a3bf84
onuorah ike <jsxike@gmail.com>
2021-03-21 21:20:43 +0100

    final3

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+  //PASSWORD MATCHING
+  $('#password, #confirm_password').on('keyup', function () {
+    if ($('#password').val() == $('#confirm_password').val()) {
+      $('#confirmverify').html('Matching').css('color', 'green');
+    } else $('#confirmverify').html('Not Matching').css('color', 'red');
+  });
+
+  //PASSWORD SHOW/HIDE TOGGLE
   const changeEye = () => {
     const pass = document.getElementById('my_pass');
     const hide1 = document.getElementById('hide1');
@@ -15,11 +23,32 @@ $(document).ready(function () {
   };
   $('.password_eye').on('click', changeEye);
 
-  const myInput = document.getElementById('my_pass');
-  myInput.onfocus = function () {
-    document.getElementById('message').style.display = 'inline-block';
-  };
-  myInput.onblur = function () {
-    document.getElementById('message').style.display = 'none';
-  };
+  ///FIELDS VERIFY
+  $('#name').on('focus', () => {
+    $('#nameverify').html('minimum 7 characters').css('color', 'green');
+  });
+  $('#name').on('blur', () => {
+    $('#nameverify').html('');
+  });
+
+  $('#email').on('focus', () => {
+    $('#emailverify').html('must follow standard mail format e.g example@mail.com').css('color', 'green');
+  });
+  $('#email').on('blur', () => {
+    $('#emailverify').html('');
+  });
+
+  $('#password').on('focus', () => {
+    $('#passverify').html('aleast a letter, number, uppercase and minimum 8 characters').css('color', 'green');
+  });
+  $('#password').on('blur', () => {
+    $('#passverify').html('');
+  });
+  // const myInput = document.getElementById('password');
+  // myInput.onfocus = function () {
+  //   document.getElementById('message').style.display = 'inline-block';
+  // };
+  // myInput.onblur = function () {
+  //   document.getElementById('message').style.display = 'none';
+  // };
 });
